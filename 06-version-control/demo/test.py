5d4e0e207c837fefe015882077e58b11ecac0d7e
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2021-08-08 23:45:09 +0800

    update test.py

--- 06-version-control/demo/test.py
+++ 06-version-control/demo/test.py
@@ -3,8 +3,14 @@ import sys
 def default():
     print('hello world')
 
+def bugfix():
+    print('bug fixed!')
+
 def main():
-    default()
+    if sys.argv[1] == 'bugfix':
+        bugfix()
+    else:
+        default()
 
 if __name__ == '__main__':
     main()
